74c76429655d0e80a3dea7c52336ecbee0cb9402
Jonathan Potter <jonathan@mrjonathanpotter.com>
2022-03-22 10:31:45 -0700

    commit tree 202caa5fa7709a4a72a7d3b63f258128e33ca406

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  post "/signup", to: "user#create"
+  get "/me", to: "user#show"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
